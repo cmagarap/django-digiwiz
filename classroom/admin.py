cf816ac87d92adfbb0333b9af07bc7d82a7557b3
Seej <seej@acudeen.com>
2019-05-17 19:44:44 +0800

    added files and updated gitignore

---
+++ classroom/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(User)
+admin.site.register(Subject)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Student)
+admin.site.register(TakenQuiz)
+admin.site.register(StudentAnswer)
